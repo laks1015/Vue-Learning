35025c0c628e0fb216a8614b965bd28011f6a99f
Laks <laks1015@gmail.com>
2025-03-11 22:29:34 +0200

    Data Binding Section2/Lesson33

--- Practice resources/basics-05-using-the-native-event-object/app.js
+++ Practice resources/basics-05-using-the-native-event-object/app.js
@@ -7,7 +7,7 @@ const app = Vue.createApp({
   },
   methods: {
     setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
@@ -15,7 +15,23 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
-    }
+    },
+
+    resetInput(){
+      // we want to reset the input value of the input box
+      this.name ='';
+      // now resetting this.name will also update wherever we output {{name}}
+    },
+
+    outputFullName(){
+      // here is a better way to output a name + last name
+      if(this.name === ''){
+        return '';
+      }
+      
+      return this.name + ' ' + 'Pucci';
+  }
+
   }
 });
 
