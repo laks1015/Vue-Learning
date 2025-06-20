9dc8ceb248c94f41d5028e3388a7eac15cf09e48
Laks <laks1015@gmail.com>
2025-06-20 15:43:30 +0200

    feat: implement dynamic component switching for goals management

--- Practice resources/cmp-adv-01-dynamic-components/src/App.vue
+++ Practice resources/cmp-adv-01-dynamic-components/src/App.vue
@@ -1,44 +1,60 @@
 <template>
   <div>
     <the-header></the-header>
-    <!-- <TheHeader/> -->
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goals #default="slotProps">
-        <h2>{{ slotProps.item }}</h2>
-        <p>{{ slotProps.anotherProp }}</p>
-        <p>{{ slotProps['anotherProp'] }}</p>
-    </course-goals>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+
+    <!-- old way of doing a tab show/hide feature -->
+<!-- 
+    <manage-goals v-if="selectedComponent == 'manage-goals'"></manage-goals>
+    <active-goals v-if="selectedComponent == 'active-goals'"></active-goals> -->
+
+    <!-- component means - show this component if something IS true -->
+     <!-- this means that compoennt should swap itself for whatever is stored in selectedComponent witout mutliple v-if statements -->
+      <keep-alive>
+        <component v-bind:is="selectedComponent"></component>
+      </keep-alive>
+        <!-- keep alive caches the state of the component and doesnt destroy it with each change -->
+
   </div>
 </template>
 
 <script>
 
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from './components/CourseGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
 
 export default {
 // register component locally here so that it is only available in this file
   components: {
    'the-header' : TheHeader,
-   'badge-list': BadgeList,
-   'user-info': UserInfo,
-   'course-goals': CourseGoals,
+   'manage-goals': ManageGoals,
+    'active-goals': ActiveGoals,
+  //  'badge-list': BadgeList,
+  //  'user-info': UserInfo,
+  //  'course-goals': CourseGoals,
   },
   data() {
     return {
+      selectedComponent: 'active-goals',
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
         role: 'admin',
       },
     };
+  },
+
+  methods: {
+    // this method is used to change the selected component
+    // selectedComponent is a data property that is used to determine which component to show
+    setSelectedComponent(componentName) {
+      this.selectedComponent = componentName;
+    }
   },
 };
 </script>
---
+++ Practice resources/cmp-adv-01-dynamic-components/src/components/ManageGoals.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2>Manage Goals</h2>
+        <input v-model="goalInput" type="text" placeholder="Enter a new goal" />
+    </div>
+
+</template>
