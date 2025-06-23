b4b691519dc3616a9e5b92a435c7f15f2f87e09e
Laks <laks1015@gmail.com>
2025-06-23 12:56:22 +0200

    feat: initialize Vue.js project with essential files and components
    - Add package.json for project dependencies and scripts
    - Include favicon.ico for the application
    - Create index.html as the main entry point
    - Implement App.vue as the root component with resource listing
    - Add LearningResource.vue component for displaying individual resources
    - Set up main.js to bootstrap the Vue application

---
+++ Practice resources/Section-10-Course-Project/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,48 @@
+<!-- root Vue compoennt that hold all other components -->
+
+<template>
+    <div>
+        <!-- <ul v-for="resource in storedResources" :key="resource.id">
+            <li>
+                <h2>{{ resource.title }}</h2>
+                <p>{{ resource.description }}</p>
+                <a :href="resource.link" target="_blank">Visit</a>
+            </li>
+        </ul> -->
+        <ul>
+            <learning-resource v-for="resource in storedResources" 
+                :key="resource.id" 
+                :title="resource.title" 
+                :description="resource.description" 
+                :link="resource.link">
+            </learning-resource>
+        </ul>
+    </div>
+
+</template>
+
+<script>
+
+import LearningResource from './components/learning-resources/LearningResource.vue';
+export default {
+    components: {
+        'learning-resource': LearningResource
+    },
+    data() {
+        return {
+            // data properties can be defined here
+            storedResources: [{
+                id: 'official-guide',
+                title: 'Official Guide',
+                description: 'Official Vue.js documentation',
+                link: 'https://vuejs.org'
+            }, {
+                id: 'google',
+                title: 'Google',
+                description: 'Learn to Google',
+                link: 'https://Google.com'
+            }]
+        };
+    }
+}
+</script>
---
+++ Practice resources/Section-10-Course-Project/prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,35 @@
+<template>
+    <!-- we need external data to use this component so we need to make this compoennt reuable and configaruable by pasing data down into it using props-->
+    <li>
+        <div>
+            <header>
+                <h3>{{title}}</h3>
+                <button>Delete</button>
+            </header>
+            <p>{{Description}}</p>
+            <nav>
+                <a v-bind:href="link">View resource</a>
+            </nav>
+        </div>
+    </li>
+</template>
+
+<script>
+export default{
+    props: { 
+        // props are used to pass data from parent to child component
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        link: {
+            type: String,
+            required: true
+        }
+    },
+}
+</script>
