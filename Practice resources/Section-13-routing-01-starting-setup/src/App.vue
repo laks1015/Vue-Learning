4c25abdce13097bf1064c8090d612071635ff79e
Laks <laks1015@gmail.com>
2025-07-16 11:09:22 +0200

    feat: update routing setup with vue-router and implement navigation links

--- Practice resources/Section-13-routing-01-starting-setup/src/App.vue
+++ Practice resources/Section-13-routing-01-starting-setup/src/App.vue
@@ -1,24 +1,29 @@
 <template>
-  <the-navigation @set-page="setActivePage"></the-navigation>
+  <!-- <the-navigation @set-page="setActivePage"></the-navigation> -->
+  <the-navigation></the-navigation>
+
   <main>
-    <component :is="activePage"></component>
+    <!-- <component :is="activePage"></component> -->
+     <!-- we need to let vue know where to load the routed componenet -->
+      <router-view></router-view>
   </main>
 </template>
 
 <script>
-import TeamsList from './components/teams/TeamsList.vue';
-import UsersList from './components/users/UsersList.vue';
+// we dont need to import them here because we arent using it herem we are slectuing and using them in the router 
+// import TeamsList from './components/teams/TeamsList.vue';
+// import UsersList from './components/users/UsersList.vue';
 import TheNavigation from './components/nav/TheNavigation.vue';
 
 export default {
   components: {
     TheNavigation,
-    TeamsList,
-    UsersList,
+    // TeamsList,
+    // UsersList,
   },
   data() {
     return {
-      activePage: 'teams-list',
+      // activePage: 'teams-list',
       teams: [
         { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
         { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
@@ -39,11 +44,11 @@ export default {
       users: this.users,
     };
   },
-  methods: {
-    setActivePage(page) {
-      this.activePage = page;
-    },
-  },
+  // methods: {
+  //   setActivePage(page) {
+  //     this.activePage = page;
+  //   },
+  // },
 };
 </script>
 
--- Practice resources/Section-13-routing-01-starting-setup/src/main.js
+++ Practice resources/Section-13-routing-01-starting-setup/src/main.js
@@ -3,14 +3,30 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
 
 const router = createRouter({
     // history options tells the router how to manage the routing history, so when users nav back adn forth, the history makes sure the rotuer knows what the lat page was
     history: createWebHistory(),
     // set all the routes/paths/urls that ypou want to suppot here.it will be an array because there will be multiple routes
     // tells vue which vue coimpoennts should be loaded for which url
-    routes: []
+    routes: [
+        // path: is the url path that should navigate to this component
+        {path: '/teams' , component: TeamsList},
+        {path: '/users' , component: UsersList},
+    
+    ],
+    // this changes the class that is added to the active link to this cusotm class, so you take control of the active link class
+    // by default, vue adds the class 'router-link-active' to the active link,
+    linkActiveClass: 'active', // this is the class that will be added to the active link, you can change it to whatever you want
 });
-const app = createApp(App)
+
+// we n=eed to tell the app to use the router
+// this is how we register the router with the app
+// this is a global registration, so we can use the router in any component
+const app = createApp(App);
+app.use(router);
+
 
 app.mount('#app');
