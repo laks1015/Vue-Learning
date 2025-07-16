9734a316cf31a044feb2b5a0189ca759bd92c25d
Laks <laks1015@gmail.com>
2025-07-16 10:40:46 +0200

    feat: add vue-router for routing capabilities and update package dependencies

--- Practice resources/Section-13-routing-01-starting-setup/src/main.js
+++ Practice resources/Section-13-routing-01-starting-setup/src/main.js
@@ -1,7 +1,16 @@
 import { createApp } from 'vue';
+// lets register the router package so we can use it
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
 
+const router = createRouter({
+    // history options tells the router how to manage the routing history, so when users nav back adn forth, the history makes sure the rotuer knows what the lat page was
+    history: createWebHistory(),
+    // set all the routes/paths/urls that ypou want to suppot here.it will be an array because there will be multiple routes
+    // tells vue which vue coimpoennts should be loaded for which url
+    routes: []
+});
 const app = createApp(App)
 
 app.mount('#app');
