c228b4cabd8a87c4fe0f0a4e75a0e3c3587e8f47
Laks <laks1015@gmail.com>
2025-08-08 16:21:14 +0200

    feat: enhance UsersList component with save changes functionality and navigation guards like afterEach, BeforeEnter, BeforeLeave. hhellp to determine what routig shuld take place on a Global and local level

--- Practice resources/Section-13-routing-01-starting-setup/src/components/users/UsersList.vue
+++ Practice resources/Section-13-routing-01-starting-setup/src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
-  <button @click="changeRoute">Confirm</button>
+  <button @click="changeRoute">Go to Teams</button>
+  <button @click="saveChangesHandler">Save changes?</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -10,6 +11,11 @@
 import UserItem from './UserItem.vue';
 
 export default {
+  data() {
+    return {
+      saveChanges: false,
+    }
+  },
   components: {
     UserItem,
   },
@@ -20,8 +26,32 @@ export default {
       this.$router.push('/teams');
       // we're baically addign a new route to the routes in main.js
     },
+    saveChangesHandler() {
+      // This method can be used to save changes if needed
+      this.saveChanges = true;
+      console.log('save changes set to true');
+    },
   },
-};
+  unmounted() {
+    // This method can be used to clean up if needed
+    console.log('UsersList component is unmounted');
+  },
+  
+  beforeRouteLeave(to, from, next) {
+    // This guard only works if the component is used directly in a route view (not as a nested/child component)
+    console.log('beforeRouteLeave', to, from);
+    if(this.saveChanges) {
+      // if saveChanges is true, we can allow the navigation
+      next();
+    } else {
+      // if saveChanges is false, we can cancel the navigation
+      const answer = window.confirm('You have unsaved changes, do you really want to leave?');
+      // if the user confirms, we can allow the navigation
+      next(answer);
+    }
+  
+  }
+  }
 </script>
 
 <style scoped>
